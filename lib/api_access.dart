6fb535535b0406548cad2bf8926a9b0c2a13b9da
Raphaël Duhen <raphael.duhen.nexus@gmail.com>
2021-07-17 03:35:36 +0200

    feat(api): API response handling halfway done.

--- lib/api_access.dart
+++ lib/api_access.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-Future<List<String>> getQueryResult(String query) async {
+Future<List<QueryResult>> getQueryResults(String query) async {
   http.Response response = await http.get(
     Uri.parse('https://reykunyu.wimiso.nl/api/fwew?tìpawm=$query'),
   );
@@ -9,37 +9,62 @@ Future<List<String>> getQueryResult(String query) async {
   final data = jsonDecode(response.body);
 
   if (data.length == 0) {
-    return [];
+    throw NullThrownError;
   } else if (data.length == 1) {
-    return singleWordQueryResult(data[0]['sì\'eyng'], data[0]['aysämok']);
+    var queryResult =
+        singleWordQueryResult(data[0]['sì\'eyng'], data[0]['aysämok']);
+    // var converted = [
+    //   QueryResult(
+    //     navi: data[0]['sì\'eyng'][0]['na\'vi'],
+    //     type: data[0]['sì\'eyng'][0]['type'],
+    //     pronunciation: data[0]['sì\'eyng'][0]['pronunciation'],
+    //     infixes: data[0],
+    //   )
+    // ];
+    return queryResult;
   } else {
+    print("Not Implemented");
     return multiWordQueryResult(data, query);
   }
 }
 
-List<String> singleWordQueryResult(
+List<QueryResult> singleWordQueryResult(
     List<dynamic> result, List<String> suggestions) {
   if (result.length == 0) {
-    if (suggestions.length != 0) {
-      return suggestions;
-    } else {
-      return ["No results found."];
-    }
+    return [];
   }
 
-  List<String> queryResult = [];
+  List<QueryResult> queryResult = [];
 
   for (int i = 0; i < result.length; i++) {
     var res = result[i];
-    //TODO Handle each result
+
+    queryResult.add(
+      QueryResult(
+        navi: SpecialString(text: lemmaForm(res['na\'vi'], res['type'])),
+        type: SpecialString(text: toReadableType(res['type'])),
+        pronunciation:
+            pronunciationToSpecialStrings(res['pronunciation'], res['type']),
+        infixes: res.containsKey('infixes')
+            ? SpecialString(
+                text: res['infixes'].toString().replaceAll('.', '·'))
+            : null,
+        status: res.containsKey('status')
+            ? SpecialString(text: ':warning: ' + res['status'])
+            : null,
+        //TODO Implement conjugation function and auxillaries
+        // conjugation: res.containsKey('conjugated')
+        //\     ? conjugation(res['conjugated'])
+        //     : null,
+      ),
+    );
   }
 
   return queryResult;
 }
 
-List<String> multiWordQueryResult(List<dynamic> result, String query) {
-  //! Not implemented
-  return [];
+List<QueryResult> multiWordQueryResult(List<dynamic> result, String query) {
+  throw UnimplementedError;
 }
 
 String lemmaForm(String word, String type) {
@@ -53,4 +78,98 @@ String lemmaForm(String word, String type) {
     return '-' + word;
   }
   return word;
+}
+
+String toReadableType(String type) {
+  const Map<String, String> mapping = {
+    "n": "n.",
+    "n:unc": "n.",
+    "n:si": "v.",
+    "n:pr": "prop. n.",
+    "pn": "pn.",
+    "adj": "adj.",
+    "num": "num.",
+    "adv": "adv.",
+    "adp": "adp.",
+    "adp:len": "adp+",
+    "intj": "intj.",
+    "part": "part.",
+    "conj": "conj.",
+    "ctr": "sbd.",
+    "v:?": "v.",
+    "v:in": "vin.",
+    "v:tr": "vtr.",
+    "v:m": "vm.",
+    "v:si": "v.",
+    "v:cp": "vcp.",
+    "phr": "phr.",
+    "inter": "inter.",
+    "aff:pre": "pref.",
+    "aff:in": "inf.",
+    "aff:suf": "suf."
+  };
+
+  return mapping[type] ?? "E0x01";
+}
+
+/// Transforms a [pronunciation], which contains a string with dash-separated
+/// syllables and a number indicating which syllable is stressed, into special
+/// strings for text formatting using the word [type].
+List<SpecialString> pronunciationToSpecialStrings(
+    List<dynamic> pronunciation, String type) {
+  if (pronunciation.isEmpty) {
+    return [SpecialString(text: "Unknown stress")];
+  }
+
+  List<SpecialString> ret = [];
+  List<String> syllables = pronunciation[0].split('-');
+
+  for (int i = 0; i < syllables.length; i++) {
+    if (i > 0) {
+      ret.add(SpecialString(text: '-'));
+    }
+    if (syllables.length > 1 && i + 1 == pronunciation[1]) {
+      ret.add(SpecialString(text: syllables[i], underlined: true));
+    } else {
+      ret.add(SpecialString(text: syllables[i]));
+    }
+  }
+
+  if (type == "n:si") {
+    ret.add(SpecialString(text: " si"));
+  }
+
+  return ret;
+}
+
+class QueryResult {
+  final SpecialString navi;
+  final SpecialString type;
+  final List<SpecialString> pronunciation;
+  final SpecialString? infixes;
+  final SpecialString? status;
+  final SpecialString? conjugation;
+
+  QueryResult({
+    required this.navi,
+    required this.type,
+    required this.pronunciation,
+    this.infixes,
+    this.status,
+    this.conjugation,
+  });
+}
+
+class SpecialString {
+  String text;
+  final bool bold;
+  final bool italic;
+  final bool underlined;
+
+  SpecialString({
+    this.text = "",
+    this.bold = false,
+    this.italic = false,
+    this.underlined = false,
+  });
 }
