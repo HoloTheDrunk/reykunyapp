057aa46436d59ce5b4967480ad5a0105b9c4a468
Raphaël Duhen <raphael.duhen.nexus@gmail.com>
2021-07-21 17:47:20 +0200

    feat(front): Basic card layout.

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Reykunyapp',
       home: HomePage(title: 'Reykunyapp'),
-      theme: ThemeData.dark(),
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        textTheme: TextTheme(),
+      ),
       debugShowCheckedModeBanner: false,
     );
   }
@@ -81,6 +84,7 @@ class _HomePageState extends State<HomePage> {
                     Divider(
                       indent: 10,
                       endIndent: 10,
+                      thickness: 3.0,
                     )
                 // SearchResultRaw(json: queryResults[i].navi.text),
               ],
@@ -145,15 +149,197 @@ class QueryResultCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Text(queryResult.navi.text),
-          Text(queryResult.translation.text),
-          for (int i = 0; i < (queryResult.affixes?.length ?? 0); i += 2)
-            Text(
-                '${queryResult.affixes![i].text}: ${queryResult.affixes![i + 1].text}'),
-        ],
+    TextSpan pronunciation = TextSpan(children: []);
+    for (int i = 0; i < queryResult.pronunciation.length; i++) {
+      pronunciation.children?.add(
+        TextSpan(
+          text: queryResult.pronunciation[i].text,
+          style: TextStyle(
+              decoration: queryResult.pronunciation[i].underlined
+                  ? TextDecoration.underline
+                  : TextDecoration.none),
+        ),
+      );
+    }
+    return Padding(
+      padding: const EdgeInsets.only(left: 16.0, right: 16.0, bottom: 8.0),
+      child: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text.rich(
+              TextSpan(
+                text: queryResult.navi.text + ' ',
+                style: Theme.of(context).textTheme.headline4,
+                children: [
+                  TextSpan(
+                    text: queryResult.type.text,
+                    style: Theme.of(context).textTheme.headline6,
+                  )
+                ],
+              ),
+            ),
+            Text.rich(
+              pronunciation,
+            ),
+            LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+                return Divider(
+                  endIndent: constraints.maxWidth * 0.9,
+                );
+              },
+            ),
+            Text.rich(
+              TextSpan(
+                text: queryResult.translation.text,
+                style: Theme.of(context).textTheme.headline5,
+              ),
+            ),
+            if (queryResult.meaningNote != null)
+              Text.rich(
+                TextSpan(
+                  text: queryResult.meaningNote?.text,
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+              ),
+            LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+                return Divider(
+                  endIndent: constraints.maxWidth * 0.9,
+                );
+              },
+            ),
+            if (queryResult.affixes?.isNotEmpty ?? false)
+              Text("Affixes: ", style: Theme.of(context).textTheme.headline6),
+            for (int i = 0; i < (queryResult.affixes?.length ?? 0); i += 2)
+              Text(
+                '${queryResult.affixes![i].text}: ${queryResult.affixes![i + 1].text}',
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+            if (queryResult.declensions != null)
+              Table(
+                defaultColumnWidth: FlexColumnWidth(),
+                columnWidths: {0: FractionColumnWidth(.15)},
+                children: [
+                  TableRow(
+                    children: [
+                      Container(),
+                      for (String plurality in [
+                        'singular',
+                        'dual',
+                        'trial',
+                        'plural'
+                      ])
+                        Text(plurality,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 14))
+                    ],
+                  ),
+                  for (int i = 0; i < queryResult.declensions![0].length; i++)
+                    TableRow(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 8.0),
+                          child: Text(
+                            [
+                              'subjective',
+                              'agentive',
+                              'patientive',
+                              'dative',
+                              'genitive',
+                              'topical'
+                            ][i],
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 14),
+                            textAlign: TextAlign.right,
+                          ),
+                        ),
+                        for (int j = 0;
+                            j < (queryResult.declensions?.length ?? 0);
+                            j++)
+                          // Text("lmao", style: TextStyle(color: randomColor())),
+                          RichText(
+                            textScaleFactor: 0.8,
+                            text: parseDeclension(
+                                queryResult.declensions![j][i].text),
+                          )
+                      ],
+                    ),
+                ],
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+TextSpan parseDeclension(String declension) {
+  TextSpan output = TextSpan(children: []);
+
+  RegExp matchPrefix = RegExp(r'\(?([a-zìäé]*)-\)?');
+  RegExp matchLenition = RegExp(r'(?<=\{)(.*)(?=\})');
+  RegExp matchRoot = RegExp(r'(?<=[^a-zìäé])([a-zìäé]*)(?=\-)');
+  RegExp matchSuffix = RegExp(r'-[a-zìäé]*');
+
+  String? prefix = matchPrefix.firstMatch(declension)?.group(0) ?? "";
+  String? lenition = matchLenition.firstMatch(declension)?.group(0) ?? "";
+  String? root = matchRoot.allMatches(declension).isNotEmpty
+      ? matchRoot.allMatches(declension).last.group(0)
+      : "";
+  String? suffix = matchSuffix.allMatches(declension).isNotEmpty
+      ? matchSuffix.allMatches(declension).last.group(0)
+      : "";
+
+  output.children
+    ?..add(
+      TextSpan(
+        text: prefix.length > 1 ? prefix : "",
+        style: TextStyle(color: Colors.red[300]),
+      ),
+    )
+    ..add(
+      TextSpan(
+        text: lenition,
+        style: TextStyle(color: Colors.green[300]),
+      ),
+    )
+    ..add(
+      TextSpan(
+        text: root,
+        style: TextStyle(color: Colors.grey[300]),
+      ),
+    )
+    ..add(
+      TextSpan(
+        text: (suffix?.length ?? 0) > 1 ? suffix : "",
+        style: TextStyle(color: Colors.blue[300]),
+      ),
+    );
+
+  return output;
+}
+
+// Placeholders
+Color randomColor() {
+  Random rand = Random();
+  return Color.fromARGB(
+      255, rand.nextInt(255), rand.nextInt(255), rand.nextInt(255));
+}
+
+class SpecialText extends StatelessWidget {
+  final SpecialString data;
+  const SpecialText({required this.data, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      data.text,
+      style: TextStyle(
+        fontWeight: data.bold ? FontWeight.bold : FontWeight.normal,
+        fontStyle: data.italic ? FontStyle.italic : FontStyle.normal,
+        decoration:
+            data.underlined ? TextDecoration.underline : TextDecoration.none,
       ),
     );
   }
